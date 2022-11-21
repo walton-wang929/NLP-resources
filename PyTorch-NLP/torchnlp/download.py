6de38fd9351a375d10de7049dd8b9129509608c5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 23:47:53 +0000

    Adding tarfile member sanitization to extractall()

--- PyTorch-NLP/torchnlp/download.py
+++ PyTorch-NLP/torchnlp/download.py
@@ -102,7 +102,26 @@ def _maybe_extract(compressed_filename, directory, extension=None):
             zip_.extractall(directory)
     elif 'tar' in extension or 'tgz' in extension:
         with tarfile.open(compressed_filename, mode='r') as tar:
-            tar.extractall(path=directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=directory)
 
     logger.info('Extracted {}'.format(compressed_filename))
 
